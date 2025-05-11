160a17824c4be2984b6d4e9657984fb1f79bb46e
tunmdev <tunmdevv@gmail.com>
2025-05-11 17:08:43 +0700

    Add bottom navigation bar

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -24,27 +24,24 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Pick your category')),
-      body: GridView(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children:
-            availableCategories
-                .map(
-                  (category) => CategoryGridItem(
-                    category: category,
-                    onSelectCategory: () {
-                      _selectCategory(context, category);
-                    },
-                  ),
-                )
-                .toList(),
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
+      children:
+          availableCategories
+              .map(
+                (category) => CategoryGridItem(
+                  category: category,
+                  onSelectCategory: () {
+                    _selectCategory(context, category);
+                  },
+                ),
+              )
+              .toList(),
     );
   }
 }
---
+++ lib/screens/tabs.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:meals/screens/meals.dart';
+import 'package:meals/screens/categories.dart';
+
+class TabsScreen extends StatefulWidget {
+  const TabsScreen({super.key});
+
+  @override
+  State<TabsScreen> createState() {
+    return _TabsScreenState();
+  }
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget activePage = const CategoriesScreen();
+    var activePageTitle = 'Category';
+
+    if (_selectedPageIndex == 1) {
+      activePage = MealsScreen(meals: []);
+      activePageTitle = 'Your Favorites';
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: Text(activePageTitle)),
+
+      body: activePage,
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.set_meal),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
+        ],
+        onTap: _selectPage,
+        currentIndex: _selectedPageIndex,
+      ),
+    );
+  }
+}
